d0e35a15b93c4875334b2b21328dd152e13abea3
flamestro <19ahmet99@gmail.com>
2020-06-06 12:01:45 +0200

    Add hover effect for desktop

--- src/components/MenuBar/MenuItem.js
+++ src/components/MenuBar/MenuItem.js
@@ -10,10 +10,37 @@ const MenuItemElement = styled.a`
     color: white !important;
     font-size: 25px;
     background-color:  #F0A856;
+    position: relative;
+           
     @media (min-width: 600px) {
+        transition: background-color 1s;
         border: solid 2px black;
         display: inline-block;
         width: ${props => 100 / props.menuItemAmount}%;
+        &:before {
+                background-color: #F0A856;
+                content: '';
+                display:block;
+                position:absolute;
+                width: 0;
+                left: 0;
+                height: 2px;
+                top: 80%;
+                transition: width 0.5s, left 0.5s, background-color 1s;
+              }
+        &:hover {
+          background-color: #F0AA0A;
+          &:before {
+                content: '';
+                display:block;
+                position:absolute;
+                width: 50%;
+                left: 25%;
+                height: 2px;
+                background-color: white;
+                top: 80%;
+              }
+        }
     }
 `
 export const MenuItem = (props) =>{
